07d7729c3a9acf9e2a5c826a37d57072a07163be
Mateus <atrujillomateus@gmail.com>
2021-08-12 16:28:56 -0500

    Innit commit. Pending modifications.

---
+++ footer.php
@@ -0,0 +1,11 @@
+        <script src="js/jquery-2.2.4.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <!-- <script src="js/jquery.easing.min.js"></script> -->
+        <script src="js/isotope.pkgd.min.js"></script>
+        <script src="js/jquery.nicescroll.min.js"></script>
+        <script src="js/owl.carousel.min.js"></script>
+        <script src="js/jquery-validation.min.js"></script>
+        <script src="js/form.min.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+</html>
