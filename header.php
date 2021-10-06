1753213bb8906ba8bc5ed586b6927ab802eeb99d
Mateus <atrujillomateus@gmail.com>
2021-10-06 04:56:49 -0500

    Stardarized header and footer.

---
+++ header.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<?php
+if (isset($_GET['lang'])) {
+    if ($_GET['lang'] == 'es' || $_GET['lang'] == 'en') {
+        require("./lang/lang_" . $_GET['lang'] . ".php");
+        setcookie('lang', $_GET['lang'], time() + 31536000, '/', '', false, false);
+        echo "<html lang='" . $_GET['lang'] . "' dir='ltr'>";
+        $lang = $_GET['lang'];
+    } else {
+        setcookie('lang', 'es', time() + 31536000, '/', '', false, false);
+        //echo '<script type="text/javascript"> window.location = window.location.pathname; </script>';
+    }
+} else if (isset($_COOKIE['lang'])) {
+    if ($_COOKIE['lang'] == 'es' || $_COOKIE['lang'] == 'en') {
+        require("./lang/lang_" . $_COOKIE['lang'] . ".php");
+        echo "<html lang='" . $_COOKIE['lang'] . "' dir='ltr'>";
+        $lang = $_COOKIE['lang'];
+    } else {
+        setcookie('lang', 'es', time() + 31536000, '/', '', false, false);
+        //echo '<script type="text/javascript"> window.location = window.location.pathname; </script>';
+    }
+} else {
+    setcookie('lang', 'es', time() + 31536000, '/', '', false, false);
+    require("./lang/lang_es.php");
+    echo "<html lang='es' dir='ltr'>";
+    //echo '<script type="text/javascript"> window.location = window.location.pathname; </script>';
+}
+if (isset($_GET['title'])) {
+    $title = "COSMIC Studios - " . $_GET['title'];
+} else {
+    $title = "COSMIC Studios";
+}
+?>
+
+<head>
+    <meta charset="utf-8">
+    <title><?= $title; ?></title>
+    <!-- Facebook and Twitter integration -->
+    <meta property="og:title" content="COSMIC Studios" />
+    <meta property="og:type" content="website" />
+    <meta property="og:image" content="http://cosmicstudios.co/img/logo.jpg" />
+    <meta property="og:url" content="http://cosmicstudios.co/" />
+    <meta property="og:site_name" content="cosmicstudios.co" />
+    <meta property="og:description" content="Empresa productora de medios y radiodifusión. Productora de fotografía y vídeo profesional." />
+    <!-- Meta tags -->
+    <meta http-equiv="Content-Language" content="es,en" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="Empresa productora de medios y radiodifusión. Productora de fotografía y vídeo profesional." />
+    <meta name="keywords" content="COSMIC Studios, cosmicstudios.co, producción, productora, producción audiovisual, medios, fotografía" />
+    <meta name="author" content="Mateus [byUwUr]" />
+    <meta name="copyright" content="Mateus [byUwUr]" />
+    <meta name="theme-color" content="#006" />
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+    <link rel="shortcut icon" type="image/png" href="img/favicon.png" />
+    <link rel="icon" type="image/png" href="img/favicon.png" />
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Play:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+
+<body>
+    <!-- Page Preloder -->
+    <div id="preloder">
+        <div class="loader"></div>
+    </div>
+
+    <!-- Header Section Begin -->
+    <header class="header">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2">
+                    <div class="header__logo">
+                        <a href="./index.html"><img src="img/logo.png" alt=""></a>
+                    </div>
+                </div>
+                <div class="col-lg-10">
+                    <div class="header__nav__option">
+                        <nav class="header__nav__menu mobile-menu">
+                            <ul>
+                                <li class="active"><a href="./index.html">Home</a></li>
+                                <li><a href="./about.html">About</a></li>
+                                <li><a href="./portfolio.html">Portfolio</a></li>
+                                <li><a href="./services.html">Services</a></li>
+                                <li><a href="#">Pages</a>
+                                    <ul class="dropdown">
+                                        <li><a href="./about.html">About</a></li>
+                                        <li><a href="./portfolio.html">Portfolio</a></li>
+                                        <li><a href="./blog.html">Blog</a></li>
+                                        <li><a href="./blog-details.html">Blog Details</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="./contact.html">Contact</a></li>
+                            </ul>
+                        </nav>
+                        <div class="header__nav__social">
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                            <a href="#"><i class="fa fa-dribbble"></i></a>
+                            <a href="#"><i class="fa fa-instagram"></i></a>
+                            <a href="#"><i class="fa fa-youtube-play"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="mobile-menu-wrap"></div>
+        </div>
+    </header>
+    <!-- Header End -->
