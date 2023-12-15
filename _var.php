a17b694a2cad786142f7a02ce4c0d9437144e107
Mateus <atrujillomateus@gmail.com>
2023-12-15 04:22:12 -0500

    Adapt router for frontend handling

--- _var.php
+++ _var.php
@@ -7,16 +7,11 @@ $PATH_DIFF = count(explode("/", str_replace("\\", "/", $_SERVER["SCRIPT_FILENAME
 $TO_HOME = $PATH_DIFF ? str_repeat("../", $PATH_DIFF) : "./";
 $THIS_PATH = str_replace("\\", "/",  dirname($PROTOCOL . $_SERVER["HTTP_HOST"] . $_SERVER["PHP_SELF"])) . "/";
 $HOME_PATH = implode("/", array_slice(explode("/", $THIS_PATH), 0, -1 - $PATH_DIFF)) . "/";
-
-// --- declarations ---
-$DNI_TYPES = [
-    1 => "Número único de identificación personal (NUIP)",
-    2 => "Registro civil (RC)",
-    3 => "Tarjeta de identidad (TI)",
-    4 => "Cédula de ciudadanía (CC)",
-    5 => "Cédula de extranjería (CE)",
-    6 => "Pasaporte (PS)",
-    9 => "Otro..."
-];
-$BLOOD_TYPES = ["O+", "O-", "A+", "A-", "B+", "B-", "AB+", "AB-"];
-$DAYS_OF_WEEK = ["--- Seleccionar día de la semana ---", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
+?>
+<script>
+    localStorage.setItem("PROTOCOL", "<?= $PROTOCOL; ?>");
+    localStorage.setItem("PATH_DIFF", "<?= $PATH_DIFF; ?>");
+    localStorage.setItem("TO_HOME", "<?= $TO_HOME; ?>");
+    localStorage.setItem("THIS_PATH", "<?= $THIS_PATH; ?>");
+    localStorage.setItem("HOME_PATH", "<?= $HOME_PATH; ?>");
+</script>
